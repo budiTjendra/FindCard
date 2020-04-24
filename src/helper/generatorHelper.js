13090db3197c271f29bff6513855fddfb2e50baf
budi-injo <budi.injo@telepathy.ai>
2020-04-24 23:46:49 +0800

    Create generatorHelper.js

---
+++ src/helper/generatorHelper.js
@@ -0,0 +1,36 @@
+
+export const generateRandomNumber = (X) => {    
+    let numArr = []
+
+    while(numArr.length <= X){
+      const num = Math.floor(Math.random() * 100)
+      if (numArr.indexOf(num) === -1){
+        numArr.push(num)
+      }  
+    }
+
+    return numArr
+}
+
+export const generatePairsInArray = (numArr) => {
+    let temp = []
+    
+    temp.push(...numArr)
+    temp.push(...numArr) 
+    
+    let pairArr = []
+    
+    while (pairArr.length !== numArr.length * 2){
+        const randomIndex = Math.floor(Math.random() * temp.length) 
+        pairArr.push(temp[randomIndex])
+        temp.splice(randomIndex, 1)
+    }
+    
+    return pairArr
+    
+}
+
+export default {
+    generatePairsInArray,
+    generateRandomNumber
+}
