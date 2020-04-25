21367ada9c99b8bc3262ffe6700f3baaf7534850
budi-injo <budi.injo@telepathy.ai>
2020-04-25 08:17:54 +0800

    convert to class component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,49 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
-import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import * as actionCreator from '../redux/actionCreators';
 
 const { increaseStepCount } = actionCreator;
 
-const Card = ({ num }) => {
-  const dispatch = useDispatch();
+class Card extends Component {
+  constructor(props) {
+    super(props);
+    const { num } = this.props;
+    this.state = { num };
+  }
 
-  const onClick = () => {
-    dispatch(increaseStepCount());
+  onClick = () => {
+    const { increaseStepCount: increaseStepCountAction } = this.props;
+
+    console.log('yoyo', this.props);
+    increaseStepCountAction();
   };
 
-  const createDummyElement = () => (
-    <View style={[styles.cardContainer, { display: 'none' }]}>
-      <Text>{num}</Text>
-    </View>
-  );
-
-  const createElement = () => (
-    <TouchableWithoutFeedback onPress={onClick}>
-      <View style={styles.cardContainer}>
+  render() {
+    const { num } = this.state;
+    const createDummyElement = () => (
+      <View style={[styles.cardContainer, { display: 'none' }]}>
         <Text>{num}</Text>
       </View>
-    </TouchableWithoutFeedback>
-  );
+    );
 
-  const element = num === 'dummy' ? createDummyElement() : createElement();
+    const createElement = () => (
+      <TouchableWithoutFeedback onPress={this.onClick}>
+        <View style={styles.cardContainer}>
+          <Text>{num}</Text>
+        </View>
+      </TouchableWithoutFeedback>
+    );
 
-  return (
-    <View
-      style={{
-        width: '30%',
-        height: undefined,
-        aspectRatio: 3 / 5,
-      }}
-    >
-      {element}
-    </View>
-  );
-};
-Card.propTypes = {
-  num: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-};
+    const element = num === 'dummy' ? createDummyElement() : createElement();
+
+    return (
+      <View
+        style={{
+          width: '30%',
+          height: undefined,
+          aspectRatio: 3 / 5,
+        }}
+      >
+        {element}
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   cardContainer: {
@@ -58,4 +65,16 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Card;
+const mapStateToProps = state => {
+  const { stepCount } = state;
+  return {
+    stepCount,
+  };
+};
+
+Card.propTypes = {
+  num: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  increaseStepCount: PropTypes.func,
+};
+
+export default connect(mapStateToProps, { increaseStepCount })(Card);
